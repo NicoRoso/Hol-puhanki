4873eebb886aebce08d66ebafef422e353dcf877
OlegMorev <proks200413@gmail.com>
2024-04-27 19:02:10 +0300

    skeleton base

---
+++ HolProject/Assets/Scripts/EnemyBehaviour/Skeleton/SkeletonBaseState.cs
@@ -0,0 +1,5 @@
+public abstract class SkeletonBaseState
+{
+    abstract public void EnterState(SkeletonStateManager skelet);
+    abstract public void UpdateState(SkeletonStateManager skelet);
+}
---
+++ HolProject/Assets/Scripts/EnemyBehaviour/Skeleton/States/SkeletonIdleState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonIdleState : SkeletonBaseState
+{
+    public override void EnterState(SkeletonStateManager skelet)
+    {
+
+    }
+    public override void UpdateState(SkeletonStateManager skelet)
+    {
+
+    }
+}
