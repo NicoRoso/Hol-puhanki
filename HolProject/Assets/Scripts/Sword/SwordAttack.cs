f8ff9f285f67e7ec263b8a2038b5b343e12191ff
El_Nico <g.nabiulin@yandex.ru>
2024-04-27 11:06:38 +0300

    SwordAttackChecker

---
+++ HolProject/Assets/Scripts/Sword/SwordAttack.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public static Action OnAttacked;
+
+    private void OnEnable()
+    {
+        OnAttacked += AttackChecker;
+    }
+
+    private void OnDisable()
+    {
+        OnAttacked -= AttackChecker;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            OnAttacked?.Invoke();
+        }
+    }
+
+    private void AttackChecker()
+    {
+        Debug.Log("Attacked function");
+    }
+}
