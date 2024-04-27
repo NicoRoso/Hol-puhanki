093a6c0192fe05389605301ba003e8fa3ed3167d
OlegMorev <proks200413@gmail.com>
2024-04-27 21:05:04 +0300

    hp bar on boss and skeleton

--- HolProject/Assets/Scripts/EnemyBehaviour/CavesBoss/BossHealth.cs
+++ HolProject/Assets/Scripts/EnemyBehaviour/CavesBoss/BossHealth.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
     [SerializeField] int _maxHealth;
     [SerializeField] float _iFrameDuration;
     [SerializeField] int maxHitsInARow;
+    [SerializeField] Slider _hpBar;
     bool damageable = true;
     int health;
     int hitsInARow = 0;
@@ -15,13 +17,16 @@ public class BossHealth : MonoBehaviour
     {
         health = _maxHealth;
         boss = GetComponent<BossStateManager>();
+        ShowHP(false);
     }
     public void TakeDamage(int damage)
     {
         if (!damageable) return;
         health -= damage;
+        ShowHP(true);
         if (health <= 0)
         {
+            ShowHP(false);
             health = 0;
             boss.SwitchState(boss.bossDeath);
             return;
@@ -42,5 +47,13 @@ public class BossHealth : MonoBehaviour
         damageable = false;
         yield return new WaitForSeconds(_iFrameDuration);
         damageable = true;
+    }
+    private void Update()
+    {
+        _hpBar.value = health / _maxHealth;
+    }
+    void ShowHP(bool visible)
+    {
+        _hpBar.gameObject.SetActive(visible);
     }
 }
