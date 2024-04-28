ddabd2123f14d6a7286d07eb17052b196db2c4b4
El_Nico <118479527+NicoRoso@users.noreply.github.com>
2024-04-28 10:44:33 +0300

    Menu Half Is Ready

--- HolProject/Assets/Scripts/Sword/SwordAttack.cs
+++ HolProject/Assets/Scripts/Sword/SwordAttack.cs
@@ -5,26 +5,11 @@ public class SwordAttack : MonoBehaviour
 {
     public static Action OnAttacked;
 
-    private void OnEnable()
-    {
-        OnAttacked += AttackChecker;
-    }
-
-    private void OnDisable()
-    {
-        OnAttacked -= AttackChecker;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Respawn")
         {
             OnAttacked?.Invoke();
         }
-    }
-
-    private void AttackChecker()
-    {
-        Debug.Log("Attacked function");
     }
 }
